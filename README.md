c534b230d589e07d46b005075f695cc17dde2052
Benny Carlsson <benny.carlsson@humblebee.se>
2025-03-28 14:13:12 +0100

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+# Planty
+
+![Demo](demo.gif)
+
+Planty is an Electron application that helps you remember to water your plant. It runs in the macOS status bar and changes its icon daily.
+
+<img src="image.jpeg" alt="Image" width="400">
+
+## Icons
+
+| Day 1               | Day 2               | Day 3               | Day 4               | Day 5               | Day 6               |
+| ------------------- | ------------------- | ------------------- | ------------------- | ------------------- | ------------------- |
+| ![Day 1](icon1.png) | ![Day 2](icon2.png) | ![Day 3](icon3.png) | ![Day 4](icon4.png) | ![Day 5](icon5.png) | ![Day 6](icon6.png) |
+
+## Installation
+
+Install dependencies:
+
+```sh
+yarn install
+```
+
+Start the application:
+
+```sh
+yarn start
+```
+
+## Building
+
+To build the application into a distributable format:
+
+Build the application:
+
+```sh
+yarn build
+```
+
+The distributable package will be created in the `dist` directory.
---
+++ main.js
@@ -0,0 +1,76 @@
+const { app, Tray, Menu, nativeImage, powerMonitor } = require("electron");
+const path = require("path");
+const cron = require("node-cron");
+
+let tray = null;
+let changeIconInterval = null;
+let isChangingIcons = false;
+
+app.on("ready", () => {
+  if (app.dock) {
+    app.dock.hide();
+  }
+
+  app.setLoginItemSettings({
+    openAtLogin: true,
+    openAsHidden: true,
+  });
+
+  const icons = [
+    nativeImage.createFromPath(path.join(__dirname, "icon1.png")).resize({ width: 16, height: 16 }),
+    nativeImage.createFromPath(path.join(__dirname, "icon2.png")).resize({ width: 16, height: 16 }),
+    nativeImage.createFromPath(path.join(__dirname, "icon3.png")).resize({ width: 16, height: 16 }),
+    nativeImage.createFromPath(path.join(__dirname, "icon4.png")).resize({ width: 16, height: 16 }),
+    nativeImage.createFromPath(path.join(__dirname, "icon5.png")).resize({ width: 16, height: 16 }),
+    nativeImage.createFromPath(path.join(__dirname, "icon6.png")).resize({ width: 16, height: 16 }),
+  ];
+
+  tray = new Tray(icons[0]);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Water Plant",
+      type: "normal",
+      click: () => {
+        if (isChangingIcons) {
+          clearInterval(changeIconInterval);
+          isChangingIcons = false;
+          updateIcon();
+        } else {
+          let index = 0;
+          changeIconInterval = setInterval(() => {
+            tray.setImage(icons[index]);
+            index = (index + 1) % icons.length;
+          }, 1000); // Change icon every 3 seconds
+          isChangingIcons = true;
+        }
+      },
+    },
+    {
+      label: "Quit",
+      type: "normal",
+      click: () => {
+        app.quit();
+      },
+    },
+  ]);
+  tray.setToolTip("Planty - Remember to water your plant");
+  tray.setContextMenu(contextMenu);
+
+  const updateIcon = () => {
+    const startDate = new Date(2025, 1, 20);
+    const today = new Date();
+    const diffTime = Math.abs(today - startDate);
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+    const iconIndex = diffDays % icons.length;
+    console.log("sets icon", icons[iconIndex]);
+    tray.setImage(icons[iconIndex]);
+  };
+
+  updateIcon();
+
+  // Run updateIcon when the Mac wakes up from sleep
+  powerMonitor.on("resume", () => {
+    updateIcon();
+  });
+});
